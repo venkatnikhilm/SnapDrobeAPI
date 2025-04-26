1dc8e80f76bbe6ad2dc95fe1808da970e8bd2836
Suprad Parashar <suprad.s.parashar@gmail.com>
2025-04-26 13:50:44 -0700

    Add add_image endpoint

--- main.py
+++ main.py
@@ -4,10 +4,40 @@ from google import genai
 from google.genai import types
 from uagents import Agent, Context, Model
 from dotenv import load_dotenv
+import json
+from pydantic import BaseModel
+import boto3
+from uuid import uuid4
 
 load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+dynamo_client = boto3.client(
+    "dynamodb",
+    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+
+class Article(BaseModel):
+    main_category: str
+    sub_category: str
+    primary_color: str
+    secondary_color: str
+    other_colors: list[str]
+    pattern: str
+    description: str
+    gender: str
+    approximate_age_of_wearer: str
+    occasion: str
+    season: str
+    style: str
+    ways_to_wear: list[str]
+    image_b64: str
+
+class GeminiArticleResponse(BaseModel):
+    article: Article
+    return_response: str
 
 class ExplainRequest(Model):
     image_b64: str
@@ -22,6 +52,37 @@ agent = Agent(
     port=8001,
     endpoint=["http://0.0.0.0:8001/submit"],
 )
+
+@agent.on_rest_post("/add_image", ExplainRequest, ExplainResponse)
+async def handle_add_image(ctx: Context, req: ExplainRequest) -> ExplainResponse:
+    img_bytes = base64.b64decode(req.image_b64)
+    tmp_path = "/tmp/cap.jpg"
+    with open(tmp_path, "wb") as f:
+        f.write(img_bytes)
+
+    uploaded = client.files.upload(file=tmp_path)
+    mind = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[
+            types.Part.from_text(text="Based on the input image, extract all the relevant information and a sassy response to say that the image has been added to the database (Basically, remembered)."),
+            types.Part.from_uri(file_uri=uploaded.uri, mime_type=uploaded.mime_type),
+        ],
+        config={
+            "response_type": "application/json",
+            "response_schema": GeminiArticleResponse
+        }
+    )
+    response = json.loads(mind.text)
+    data = response["article"]
+    data["id"] = str(uuid4())
+
+    dynamo_client.put_item(
+        TableName=os.getenv("DYNAMODB_TABLE_NAME"),
+        Item=data
+    )
+
+    return response["return_response"]
+
 
 @agent.on_rest_post("/explain", ExplainRequest, ExplainResponse)
 async def handle_explain(ctx: Context, req: ExplainRequest) -> ExplainResponse:
